10514c0a324c0c74c1b87155837ec5b6cef5fa32
Eduardo Hernández <al163984@alumnos.uacj.mx>
2019-02-23 11:29:17 -0700

    Add files via upload

---
+++ La chicharronera/La chicharronera/La chicharronera/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace La_chicharronera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "a";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.txtA.Location = new System.Drawing.Point(72, 45);
+            this.txtA.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(164, 26);
+            this.txtA.TabIndex = 2;
+            // 
+            // txtC
+            // 
+            this.txtC.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.txtC.Location = new System.Drawing.Point(72, 125);
+            this.txtC.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(164, 26);
+            this.txtC.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 125);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "c";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtB
+            // 
+            this.txtB.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.txtB.Location = new System.Drawing.Point(72, 85);
+            this.txtB.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(164, 26);
+            this.txtB.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "b";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(23, 181);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(124, 35);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtX1
+            // 
+            this.txtX1.BackColor = System.Drawing.SystemColors.Window;
+            this.txtX1.Enabled = false;
+            this.txtX1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.txtX1.Location = new System.Drawing.Point(72, 262);
+            this.txtX1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(164, 26);
+            this.txtX1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(20, 262);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "x1";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtX2
+            // 
+            this.txtX2.BackColor = System.Drawing.SystemColors.Window;
+            this.txtX2.Enabled = false;
+            this.txtX2.ForeColor = System.Drawing.Color.DarkBlue;
+            this.txtX2.Location = new System.Drawing.Point(72, 301);
+            this.txtX2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(164, 26);
+            this.txtX2.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(20, 302);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "x2";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(158, 181);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(124, 35);
+            this.btnLimpiar.TabIndex = 12;
+            this.btnLimpiar.Text = "&Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(122, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Datos";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(97, 237);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Resultados";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(306, 339);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtX2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "La Chicharronera";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ La chicharronera/La chicharronera/La chicharronera/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace La_chicharronera
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Validamos entradas
+            if (txtA.Text== "")
+            {
+                MessageBox.Show("Debe de ingresar un valor en a","Error");
+                txtA.Focus();
+                return;
+            }
+            double a;
+            try
+            {
+                a = Convert.ToDouble(txtA.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Debe de ingresar un valor numerico en a", "Error");
+                txtA.Text = "";
+                txtA.Focus();
+                return;
+            }
+            if (txtB.Text == "")
+            {
+                MessageBox.Show("Debe de ingresar un valor en b", "Error");
+                txtB.Focus();
+                return;
+            }
+            double b;
+            try
+            {
+                b = Convert.ToDouble(txtB.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Debe de ingresar un valor numerico en b", "Error");
+                txtB.Text = "";
+                txtB.Focus();
+                return;
+            }
+            if (txtC.Text == "")
+            {
+                MessageBox.Show("Debe de ingresar un valor en c", "Error");
+                txtC.Focus();
+                return;
+            }
+            double c;
+            try
+            {
+                c = Convert.ToDouble(txtC.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Debe de ingresar un valor numerico en c", "Error");
+                txtC.Text = "";
+                txtC.Focus();
+                return;
+            }
+
+            //Hacemos los cálculos
+            double x1, x2,D;
+            //Ecuacioncuadratica(a, b, c, D, out x1, out x2);
+            D = (b * b) - 4 * a * c;
+            if (D > 0)
+            {
+                x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                txtX1.Text = x1.ToString();
+                txtX2.Text = x2.ToString();
+            }
+            if (D == 0)
+            {
+                x1 = -b;
+                txtX1.Text = x1.ToString();
+                MessageBox.Show("Ecuacion con solo un resultado (la raíz es igual a cero)", caption: "Alerta");
+                txtX2.Text = "";
+            }
+            if (D < 0)
+            {
+                MessageBox.Show("No tiene solución en números reales", caption: "Alerta");
+                txtX1.Text = "";
+                txtX2.Text = "";
+            }
+            
+            
+        }
+
+       /* private void Ecuacioncuadratica(double a, double b, double c, double D, out double x1, out double x2)
+        {
+            
+            
+
+           /* x1 = (-b + Math.Sqrt(D)) / (2 * a);
+            x2 = (-b - Math.Sqrt(D)) / (2 * a);
+        }*/
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtA.Text = "";
+            txtB.Text = "";
+            txtC.Text = "";
+            txtX1.Text = "";
+            txtX2.Text = "";
+            txtA.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
